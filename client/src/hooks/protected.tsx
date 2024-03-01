f9ec7b3a0e685b8d94d27afd44bdd16e8929edbf
Olabode Omoyele <yele.olabode@gmail.com>
2024-03-01 20:33:42 +0100

    mod

--- client/src/hooks/protected.tsx
+++ client/src/hooks/protected.tsx
@@ -1,4 +1,3 @@
-import Loader from "@/components/Loader/Loader";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import { redirect } from "next/navigation";
 
@@ -7,22 +6,7 @@ interface ProtectedProps {
 }
 
 export default function Protected({ children }: ProtectedProps) {
-  const { data, isLoading } = useLoadUserQuery({});
+  const { data } = useLoadUserQuery({});
 
-  if (isLoading) {
-    // Optional: You can render a loading indicator while user data is being fetched
-    return (
-      <div>
-        <Loader />
-      </div>
-    );
-  }
-
-  // Ensure that the user object is available and properly validated
-  if (data.user) {
-    return redirect("/");
-    // Render children if user is an admin
-  } else {
-    return <>{children}</>; // Redirect to home page if user is not an admin or if user data is not available
-  }
+  return data?.user ? redirect("/") : children;
 }
