2328b4955b6b39ebe6ea5b5b13cf5c85c788d70f
Olabode Omoyele <yele.olabode@gmail.com>
2024-05-30 02:16:28 +0100

    domain

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -25,20 +25,16 @@ export const accessTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "strict",
   secure: true,
-  domain: "https://lincher-v2.onrender.com",
-  path: "/",
 };
 
 export const refreshTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "strict",
   secure: true,
-  domain: "https://lincher-v2.onrender.com",
-  path: "/",
 };
 
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
